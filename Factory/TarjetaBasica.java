e5e9a6649923c06ec4f0476fecf226f87f8198fc
Santiago Alejandro Villacis Huerta <savihuer@espol.edu.ec>
2025-06-27 11:13:32 -0500

    notacion  @Override

--- Factory/TarjetaBasica.java
+++ Factory/TarjetaBasica.java
@@ -2,6 +2,7 @@ public class TarjetaBasica implements Tarjeta{
   private ArrayList<int> costos;
   private int limiteCrediticio;
 
+  @Override
   public void cambiarSaldo(){
     //Logica para cambiar saldo
   }
