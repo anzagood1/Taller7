175062313dbdee209d3b0aa2cc7b334e7db5bc59
Santiago Alejandro Villacis Huerta <savihuer@espol.edu.ec>
2025-06-27 11:09:54 -0500

    Create TarjetaBasica.java

---
+++ Factory/TarjetaBasica.java
@@ -0,0 +1,8 @@
+public class TarjetaBasica implements Tarjeta{
+  private ArrayList<int> costos;
+  private int limiteCrediticio;
+
+  public void cambiarSaldo(){
+    //Logica para cambiar saldo
+  }
+}
