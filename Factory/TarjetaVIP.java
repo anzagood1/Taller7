4cc13fd3024976c61184aea47110f9667a1da708
Santiago Alejandro Villacis Huerta <savihuer@espol.edu.ec>
2025-06-27 11:12:18 -0500

    Create TarjetaVIP.java

---
+++ Factory/TarjetaVIP.java
@@ -0,0 +1,9 @@
+public class TarjetaVIP implements Tarjeta{
+  private ArrayList<int> costos;
+  private int limiteCrediticio;
+
+  @Override
+  public void cambiarSaldo(){
+    //Logica para cambiar saldo
+  }
+}
