8213e250c747dd05e6973b1737bdb5d3e54bf578
Santiago Alejandro Villacis Huerta <savihuer@espol.edu.ec>
2025-06-27 11:10:17 -0500

    Create TarjetaPremium.java

---
+++ Factory/TarjetaPremium.java
@@ -0,0 +1,8 @@
+public class TarjetaPremium implements Tarjeta{
+  private ArrayList<int> costos;
+  private int limiteCrediticio;
+
+  public void cambiarSaldo(){
+    //Logica para cambiar saldo
+  }
+}
